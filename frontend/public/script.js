cccb6a67a3062103049c36ee98baf4fce81e4635
Pedro Coelho <150755362+peucsr@users.noreply.github.com>
2025-09-21 07:15:45 -0300

    imgs cardapio

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,16 +1,16 @@
 import { web, TransactionType } from 'https://sdk.kleverscan.org/kleverchain-sdk-web-esm-1-0-x.js';
 
 const cardapio = [
-  { nome: "Pudim de Café", preco: 8.6, peso: "120g", imagem: "img/pudim-cafe.jpg" },
-  { nome: "Pudim de Doce de Leite", preco: 8.9, peso: "120g", imagem: "img/pudim-doce-leite.jpg" },
-  { nome: "Pudim Tradicional", preco: 7.9, peso: "120g", imagem: "img/pudim-tradicional.jpg" },
-  { nome: "Chocolate Branco c/ Calda de Caramelo", preco: 9.5, peso: "120g", imagem: "img/pudim-chocobranco-caramelo.jpg" },
-  { nome: "Chocolate Branco c/ Calda de Morango", preco: 10.6, peso: "120g", imagem: "img/pudim-chocobranco-morango.jpg" },
-  { nome: "Pudim de Coco", preco: 9.3, peso: "120g", imagem: "img/pudim-coco.jpg" },
-  { nome: "Pudim de Leite Ninho", preco: 9.1, peso: "120g", imagem: "img/pudim-ninho.jpg" },
-  { nome: "Chocolate ao Leite c/ Calda de Maracujá", preco: 9.9, peso: "120g", imagem: "img/pudim-chocolate-maracuja.jpg" },
-  { nome: "Chocolate ao Leite c/ Calda de Caramelo", preco: 9.9, peso: "120g", imagem: "img/pudim-chocolate-caramelo.jpg" },
-  { nome: "Pudim de Abacaxi", preco: 8.9, peso: "120g", imagem: "img/pudim-abacaxi.jpg" }
+  { nome: "Pudim de Café", preco: 8.6, peso: "120g", imagem: "img/pudim-cafe.svg" },
+  { nome: "Pudim de Doce de Leite", preco: 8.9, peso: "120g", imagem: "img/pudim-doce-leite.svg" },
+  { nome: "Pudim Tradicional", preco: 7.9, peso: "120g", imagem: "img/pudim-tradicional.svg" },
+  { nome: "Chocolate Branco c/ Calda de Caramelo", preco: 9.5, peso: "120g", imagem: "img/pudim-chocobranco-caramelo.svg" },
+  { nome: "Chocolate Branco c/ Calda de Morango", preco: 10.6, peso: "120g", imagem: "img/pudim-chocobranco-morango.svg" },
+  { nome: "Pudim de Coco", preco: 9.3, peso: "120g", imagem: "img/pudim-coco.svg" },
+  { nome: "Pudim de Leite Ninho", preco: 9.1, peso: "120g", imagem: "img/pudim-ninho.svg" },
+  { nome: "Chocolate ao Leite c/ Calda de Maracujá", preco: 9.9, peso: "120g", imagem: "img/pudim-chocolate-maracuja.svg" },
+  { nome: "Chocolate ao Leite c/ Calda de Caramelo", preco: 9.9, peso: "120g", imagem: "img/pudim-chocolate-caramelo.svg" },
+  { nome: "Pudim de Abacaxi", preco: 8.9, peso: "120g", imagem: "img/pudim-abacaxi.svg" }
 ];
 
 const carrinho = [];
